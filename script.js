14e6d769c165505eaafba4bb564e6c92090c9b15
Kenny Luong <luong97@hawaii.edu>
2015-10-28 11:16:38 -1000

    Added ability to go forward through multiple pics
    We're building a gallery right? That means we want people to
    be able to cycle through multiple pictures. Here, I provided some
    basic functionality - we can cycle forward through multiple pictures.
    
    This uses a couple of things that are a little more complicated;
    arrays and objects.
    
    I added an array where we can store multiple pictures and their titles.
    In each element of the array, we have a 'url' and a 'title'. They're
    pretty much self explainatory - they're the URL and the TITLE of the
    pictures you want to show.
    
    Objects are denoted by braces "{}" and arrays are denoted by square
    brackets "[]". Within them, each element is separated by a comma.
    
    Below, you can see that when the changePicture function is called,
    there are now several functions which grab the pictures using an index;
    If you remember correctly, we need an index every time we access an
    element in an array. It was simple to substitute this in for the
    code that was there before!
    
    Also note - we update the index at the bottom so that the next time
    you hit the forward button, the index updates and the browser shows the
    next picture.

--- script.js
+++ script.js
@@ -5,17 +5,31 @@
 // Goal 2: Change title and links
 // Goal 3: Include other pictures, titles and links
 
-var new_picture = "http://www.midpac.edu/mypueo/downloads/hs/MidPacific_complete_stacked%20(2).jpg"
-var new_title = "Midpack"
+
+
+var index = 0;
+var pictures = [
+                {
+                    url: "http://www.midpac.edu/mypueo/downloads/hs/MidPacific_complete_stacked%20(2).jpg",
+                    title: "Midpack"
+                },
+                {
+                    url: "http://www.hawaii-all-inclusive.net/files/3318241/uploaded/all-inclusive-waikiki-hawaii-vacation-package.jpg",
+                    title: "Hawaii"
+                }
+               ]
 
 // Function that changes the source of the main picture
 var changePicture = function(){
-    document.getElementById("main-picture").src = new_picture;
+    document.getElementById("main-picture").src = pictures[index].url;
 
-    document.getElementById("picture-title").innerHTML = new_title;
+    document.getElementById("picture-title").innerHTML = pictures[index].title;
 
-    document.getElementById("picture-link").innerHTML = new_picture;
-    document.getElementById("picture-link").href = new_picture;
+    document.getElementById("picture-link").innerHTML = pictures[index].url;
+    document.getElementById("picture-link").href = pictures[index].url;
+
+    // Update the index
+    index = index + 1;
 };
 
 
