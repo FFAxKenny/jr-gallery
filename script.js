ace390c4d16f764b6e53bddf55de7de57d553103
Kenny Luong <luong97@hawaii.edu>
2015-10-28 11:08:40 -1000

    Added ability to change titles and links
    While navigating through our gallery, we want users to be able to
    read a different title for each picture as well as have a link at the
    bottom so that they can navigate to the picture directly.
    
    The process is similar to what we did in the last commit. Here, I
    added identifiers to the picture title and the picture link so that
    while I'm writing my javascript I can modify the tags.
    
    The only change I needed to make in the javascript was to add
    functions to modify three things:
    
      * The page title
      * The page link
      * The page link text

--- script.js
+++ script.js
@@ -6,11 +6,18 @@
 // Goal 3: Include other pictures, titles and links
 
 var new_picture = "http://www.midpac.edu/mypueo/downloads/hs/MidPacific_complete_stacked%20(2).jpg"
+var new_title = "Midpack"
 
 // Function that changes the source of the main picture
 var changePicture = function(){
     document.getElementById("main-picture").src = new_picture;
+
+    document.getElementById("picture-title").innerHTML = new_title;
+
+    document.getElementById("picture-link").innerHTML = new_picture;
+    document.getElementById("picture-link").href = new_picture;
 };
+
 
 // Do changePicture() when the element 'forward' is clicked
 document.getElementById("forward").addEventListener("click", function(){
